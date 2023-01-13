c99e587a89740394388b0682dfb744ce91d92d47
revaldoi <om.revaldo@gmail.com>
2023-01-13 07:42:09 +0700

    form without component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
+import { IconPlaceholder, IconSettingsOff } from '@tabler/icons';
 import { useState } from 'react';
 import Button from './components/Button';
 import Card from './components/Card';
+import Counter from './components/Counter'
+import PlaceContentCenter from './components/PlaceContentCenter'
 // import './App.css';
 
 export default function App() {
+   const [form, setForm] = useState({
+      name: 'dede',
+      email: 'mail@domain.com'
+   })
+
+   function onChange(event){
+      setForm({...form, [event.target.name] : event.target.value})
+   }
    return (
       <div className={'bg-slate-100 antialiased flex items-center justify-center min-h-screen'}>
          <div className={'block p-6 rounded-lg shadow-lg bg-white max-w-sm'}>
             <Card>
-               <Card.Title>Halo</Card.Title>
+               <Card.Title>Sign up for new account</Card.Title>
                <Card.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  <div className={'mb-5 border rounded-lg p-4'}>
+                     <p>Name: {form.name}</p>
+                  </div>
+                  <div className={'mb-6'}>
+                     <label htmlFor='name'></label>
+                     <input value={form.name} onChange={onChange} id={'name'} name={'name'}></input>
+                  </div>
                </Card.Body>
                <Card.Footer>
                   <Button onClick={() => console.log('Halo')} type='submit'>Register</Button>
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,17 @@
-import Button from "./Button"
-import { useState } from "react"
+import Button from './Button';
+import { useState } from 'react';
 
-export default function Counter({nilai}) {
-    const [hitung, setHitung] = useState(nilai)
-      
-    function handleClick() {
-        setHitung((nilai) => nilai + 1);
-    }
+export default function Counter({ nilai }) {
+   const [hitung, setHitung] = useState(nilai);
 
+   function handleClick() {
+      setHitung((nilai) => nilai + 1);
+   }
 
-    return (
-        <div>
-            <h1>{hitung}</h1>
-            <Button onClick={handleClick}>+ 1</Button>
-        </div>
-    )
+   return (
+      <div>
+         <h1>{hitung}</h1>
+         <Button onClick={handleClick}>+ 1</Button>
+      </div>
+   );
 }
