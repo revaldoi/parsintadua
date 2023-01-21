07ef97762e227f647e3b5cf118317502524f8046
revaldoi <om.revaldo@gmail.com>
2023-01-21 12:12:56 +0700

    completed 9

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -10,11 +10,30 @@ export default function Todo() {
     function handleAddTask(e) {
         e.preventDefault();
         setNewTasks((prevTasks) => [...prevTasks, {
-            id: 1,
+            id: Math.floor(Math.random() * Date.now()),
             name: newTask,
             completed: false,
         }]);
         setNewTask('')
+    }
+
+    function handleCompleteTask(id) {
+        console.log(id)
+        const updateTask = tasks.map((task) => {
+        if (id === task.id) {
+            return {
+                ...task, 
+                completed: !task.completed
+            }
+        }
+        return task;
+        });
+        setNewTasks(updateTask)
+    }
+
+    function handleRemoveTask(id) {
+        const removeTask = tasks.filter(task => task.id !==id)
+        setNewTasks(removeTask)
     }
 
     return (
@@ -27,6 +46,21 @@ export default function Todo() {
                         <Button onClick={handleAddTask}>Add task</Button>
                     </div>
                 </form>
+                {tasks.length > 0 ? (
+                    <ol className={'space-y-2 mt-4'}>
+                        {tasks.map((task) => (
+                            <li key={task.id} className={'flex items-center justify-between'}>
+                                <span>{task.name} {task.completed ? 'Completed' : 'Uncompletted'}</span>
+                                
+                                <div className={'flex items-center gap-x-2'}>
+                                    <button onClick={() => handleCompleteTask(task.id)} className={'text-white bg-green-400 p-1 text-xs'}>
+                                        Mark as {task.completed ? 'complete' : 'incompleted'}</button>
+                                    <button onClick={() => handleRemoveTask(task.id)} className={'text-white bg-red-400 p-1 text-xs'}>Remove</button>
+                                </div>  
+                            </li>
+                        ))}
+                    </ol>
+                ) : null}
             </Card.Body>
             <Card.Footer>
                 You have {tasks.length} task
